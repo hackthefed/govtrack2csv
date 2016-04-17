7a72f67dc8c6833774b5d2cb347858bfcc1d8e33
vance <vance.dubberly@ff0000.com>
2016-04-17 14:37:24 -0700

    bump version correct email address

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(name='govtrack2csv',
-      version='1.0.1',
+      version='1.1-alpha1',
       description='Toolset for converting govtrack data to csv.',
       url='http://github.com/hackthefed/govtrack2csv',
       status='alpha',
       author='Hack The Fed',
-      author_email='vance@hackthefed.com',
+      author_email='vance@hackthefed.org',
       license='GPL',
       packages=['govtrack2csv'],
-      scripts=['bin/convert_congress'],
+      scripts=['bin/convert_congress', 'bin/extract_votes'],
       install_requires=[
           'pandas',
           'pyyaml'
